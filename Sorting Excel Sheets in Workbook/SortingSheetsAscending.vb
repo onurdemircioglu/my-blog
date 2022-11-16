4ef28ec088171d963fc7e38983043b35160fbb64
Onur Demircioğlu <59303053+onurdemircioglu@users.noreply.github.com>
2022-11-16 22:36:23 +0300

    Create SortingSheetsAscending.vb

---
+++ Sorting Excel Sheets in Workbook/SortingSheetsAscending.vb
@@ -0,0 +1,48 @@
+Option Base 1
+
+Sub SortingSheetsAscending()
+    Dim MyArray() As String
+    Dim TempVariable As String
+    
+    Dim SheetsCount As Long
+    Dim wb As Workbook
+    
+    
+    Set wb = ActiveWorkbook
+    SheetsCount = wb.Sheets.Count
+    
+    
+    'Resizing array
+    ReDim Preserve MyArray(SheetsCount) As String
+    
+    
+    For a = 1 To SheetsCount
+        MyArray(a) = wb.Sheets(a).Name
+    Next
+    
+    
+    'Sorting the array (ascending (A-Z))
+    For i = LBound(MyArray) To UBound(MyArray) 'Option Base 1 => No need to extract 1 from upper bound (Ubound)
+        For j = i + 1 To UBound(MyArray) 'Comparing previous value with other values coming after it
+            If MyArray(i) < MyArray(j) Then 'It is possible to use Ucase funtion
+                'do nothing (It is in the right place)
+            Else
+                'Switch places with the right order
+                TempVariable = MyArray(j) 'Assigning second value to temporary variable
+                MyArray(j) = MyArray(i) 'Switching places
+                MyArray(i) = TempVariable 'Re-assigning second value to first value's place
+            End If
+        Next
+    Next
+    
+    
+    'At this point all sheets are in the right order in array. All we need to move them to the right place in worksheet.
+    
+    
+    'Moving sheets to the right place
+    For i = LBound(MyArray) To UBound(MyArray) 'Option Base 1 => No need to extract 1 from upper bound (Ubound)
+        Worksheets(MyArray(i)).Move Before:=Worksheets(i)
+    Next
+    
+    Sheets("000").Select
+End Sub
